c1ef8bed703cae2d4c6e4a0bcfffc40fadb97e3b
Yurie <yurie.koga.a@gmail.com>
2020-01-21 12:39:50 -0800

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace test_Chart_AspNet_MVC_Csharp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
