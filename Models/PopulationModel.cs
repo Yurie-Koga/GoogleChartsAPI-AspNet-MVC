919d1ca55cd31e4afbcde338569a000f099260ea
Yurie <yurie.koga.a@gmail.com>
2020-01-23 15:43:25 -0800

    add chart types

--- Models/PopulationModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GoogleChartsAPI_AspNet_MVC.Models
-{
-    public class PopulationModel
-    {
-        public string CityName { get; set; }
-        public int PopulationYear2010 { get; set; }
-        public int PopulationYear2000 { get; set; }
-    }
-}
--- wwwroot/js/Chart3.js
+++ wwwroot/js/Chart3.js
@@ -14,13 +14,16 @@ $(document).ready(function() {
       toastr.error(err.message);
     },
     success: function(data) {
-      PopulationChart(data);
+      var chartData = setChartData(data);
+      BarChart(chartData, "chart_div1");
+      ColumnChart(false, chartData, "chart_div2");
+      ColumnChart(true, chartData, "chart_div3");
       return false;
     }
   });
 });
 
-function PopulationChart(data) {
+setChartData = data => {
   var dataArray = [["City", "2010 Population", "2000 Population"]];
   $.each(data, function(i, item) {
     dataArray.push([
@@ -29,7 +32,11 @@ function PopulationChart(data) {
       parseInt(item.populationYear2000)
     ]);
   });
-  var data = google.visualization.arrayToDataTable(dataArray);
+  var chartData = google.visualization.arrayToDataTable(dataArray);
+  return chartData;
+};
+
+function BarChart(data, chartId) {
   var options = {
     title: "Population of Largest U.S. Cities",
     chartArea: {
@@ -45,7 +52,29 @@ function PopulationChart(data) {
     }
   };
   var chart = new google.visualization.BarChart(
-    document.getElementById("chart_div")
+    document.getElementById(chartId)
+  );
+  chart.draw(data, options);
+}
+
+function ColumnChart(boolean, data, chartId) {
+  var options = {
+    title: "Population of Largest U.S. Cities",
+    isStacked: boolean,
+    chartArea: {
+      width: "50%"
+    },
+    colors: ["#b0120a", "#ffab91"],
+    hAxis: {
+      title: "Total Population",
+      minValue: 0
+    },
+    vAxis: {
+      title: "City"
+    }
+  };
+  var chart = new google.visualization.ColumnChart(
+    document.getElementById(chartId)
   );
   chart.draw(data, options);
 }
